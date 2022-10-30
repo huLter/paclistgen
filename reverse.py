ec275f73d33ab90092db082d64c97e6fdd856fc8
huLter <rioathe@outlook.com>
2022-10-30 14:43:38 +0800

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -10,24 +10,23 @@ with open("./pac.js", "r+") as pac_js:
 pac_js.close
 
 for js_var in pac['body']:
-    if js_var['type'] == 'VariableDeclaration' and js_var['declarations'][0]['id']['name'] == 'rules':  
-        gen_list = "[AutoProxy 0.2.9]\n"  
-        gen_list += "\n\n!------direct rules from gfwList\n\n" 
+    if js_var['type'] == 'VariableDeclaration' and js_var['declarations'][0]['id']['name'] == 'rules':
+        gen_list = f"[AutoProxy 0.2.9]\n!build on {datetime.now(timezone(timedelta(hours=8))).strftime('%y%m%d %H:%M:%S %Z')}\n"
+        gen_list += "\n\n!------direct rules from gfwList\n\n"
         for rule in js_var['declarations'][0]['init']['elements'][1]['elements'][0]['elements']:
             gen_list += f"@@{rule['value']}\n"
-        gen_list += "\n\n!------proxy rules from gfwList\n\n" 
+        gen_list += "\n\n!------proxy rules from gfwList\n\n"
         for rule in js_var['declarations'][0]['init']['elements'][1]['elements'][1]['elements']:
             gen_list += f"||{rule['value']}\n"
-        gen_list += "\n\n!------direct rules from user list\n\n" 
+        gen_list += "\n\n!------direct rules from user list\n\n"
         for rule in js_var['declarations'][0]['init']['elements'][0]['elements'][0]['elements']:
             gen_list += f"@@{rule['value']}\n"
-        gen_list += "\n\n!------proxy rules from user list\n\n" 
+        gen_list += "\n\n!------proxy rules from user list\n\n"
         for rule in js_var['declarations'][0]['init']['elements'][0]['elements'][1]['elements']:
             gen_list += f"||{rule['value']}\n"
-        gen_list += f"\n!build on {datetime.now(timezone(timedelta(hours=8))).strftime('%y%m%d %H:%M:%S %Z')}\n"
 
 with open("./merge.list", "wb+") as merge_list:
     merge_list.write(base64.b64encode(gen_list.encode(encoding="UTF-8")))
 merge_list.close
 
-pass            
+pass
